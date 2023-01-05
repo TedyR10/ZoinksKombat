dfb08a6fee90c2cb5ccfd9b38bca41171db85735
TedyR10 <theodor.rolea@stud.acs.upb.ro>
2023-01-05 15:17:42 +0200

    main changes

--- main.py
+++ main.py
@@ -5,174 +5,174 @@ from fighter import Fighter
 
 def start_game(rounds):
 
-	mixer.init()
-	pygame.font.init()
+    mixer.init()
+    pygame.font.init()
 
-	# create game window
-	SCREEN_WIDTH = 1000
-	SCREEN_HEIGHT = 600
+    # create game window
+    SCREEN_WIDTH = 1000
+    SCREEN_HEIGHT = 600
 
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	pygame.display.set_caption("ZoinksKombat")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("ZoinksKombat")
 
-	# set framerate
-	clock = pygame.time.Clock()
-	FPS = 60
+    # set framerate
+    clock = pygame.time.Clock()
+    FPS = 60
 
-	# define colours
-	RED = (255, 0, 0)
-	GREEN = (0, 255, 0)
-	WHITE = (255, 255, 255)
+    # define colours
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    WHITE = (255, 255, 255)
 
-	# define game variables
-	intro_count = 3
-	last_count_update = pygame.time.get_ticks()
-	score = [0, 0]  # player scores. [P1, P2]
-	round_over = False
-	ROUND_OVER_COOLDOWN = 2000
+    # define game variables
+    intro_count = 3
+    last_count_update = pygame.time.get_ticks()
+    score = [0, 0]  # player scores. [P1, P2]
+    round_over = False
+    ROUND_OVER_COOLDOWN = 2000
 
-	# define fighter variables
-	SHAGGY_SIZE = 100
-	SHAGGY_SCALE = 4
-	SHAGGY_OFFSET = [50, 23]
-	SHAGGY_DATA = [SHAGGY_SIZE, SHAGGY_SCALE, SHAGGY_OFFSET]
-	HORSEMAN_SIZE = 100
-	HORSEMAN_SCALE = 4
-	HORSEMAN_OFFSET = [50, 23]
-	HORSEMAN_DATA = [HORSEMAN_SIZE, HORSEMAN_SCALE, HORSEMAN_OFFSET]
+    # define fighter variables
+    SHAGGY_SIZE = 100
+    SHAGGY_SCALE = 4
+    SHAGGY_OFFSET = [50, 23]
+    SHAGGY_DATA = [SHAGGY_SIZE, SHAGGY_SCALE, SHAGGY_OFFSET]
+    HORSEMAN_SIZE = 100
+    HORSEMAN_SCALE = 4
+    HORSEMAN_OFFSET = [50, 23]
+    HORSEMAN_DATA = [HORSEMAN_SIZE, HORSEMAN_SCALE, HORSEMAN_OFFSET]
 
-	# load music and sounds
-	pygame.mixer.music.load("assets/audio/song.mp3")
-	pygame.mixer.music.set_volume(0.5)
-	pygame.mixer.music.play(-1, 0.0, 0)
-	punch_fx = pygame.mixer.Sound("assets/audio/punch.mp3")
-	punch_fx.set_volume(0.5)
-	sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
-	sword_fx.set_volume(0.5)
-	magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
-	magic_fx.set_volume(0.75)
+    # load music and sounds
+    pygame.mixer.music.load("assets/audio/song.mp3")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(-1, 0.0, 0)
+    punch_fx = pygame.mixer.Sound("assets/audio/punch.mp3")
+    punch_fx.set_volume(0.5)
+    sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+    sword_fx.set_volume(0.5)
+    magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
+    magic_fx.set_volume(0.75)
 
-	# load background image
-	bg_image = pygame.image.load(
-		"assets/images/background/background.jpg").convert_alpha()
+    # load background image
+    bg_image = pygame.image.load(
+        "assets/images/background/background.jpg").convert_alpha()
 
-	# load spritesheets
-	SHAGGY_sheet = pygame.image.load(
-		"assets/images/shaggy/Sprites/shaggy.png").convert_alpha()
-	HORSEMAN_sheet = pygame.image.load(
-		"assets/images/horseman/Sprites/horseman.png").convert_alpha()
+    # load spritesheets
+    SHAGGY_sheet = pygame.image.load(
+        "assets/images/shaggy/Sprites/shaggy.png").convert_alpha()
+    HORSEMAN_sheet = pygame.image.load(
+        "assets/images/horseman/Sprites/horseman.png").convert_alpha()
 
-	# load vicory image
-	victory_img = pygame.image.load(
-		"assets/images/icons/victory.png").convert_alpha()
+    # load vicory image
+    victory_img = pygame.image.load(
+        "assets/images/icons/victory.png").convert_alpha()
 
-	# define number of steps in each animation
-	SHAGGY_ANIMATION_STEPS = [6, 5, 3, 6, 3, 2, 3]
-	HORSEMAN_ANIMATION_STEPS = [3, 3, 1, 6, 6, 2, 5]
+    # define number of steps in each animation
+    SHAGGY_ANIMATION_STEPS = [6, 5, 3, 6, 3, 2, 3]
+    HORSEMAN_ANIMATION_STEPS = [3, 3, 1, 6, 6, 2, 5]
 
-	# define font
-	count_font = pygame.font.Font("assets/fonts/comic.TTF", 80)
-	score_font = pygame.font.Font("assets/fonts/comic.TTF", 30)
+    # define font
+    count_font = pygame.font.Font("assets/fonts/comic.TTF", 80)
+    score_font = pygame.font.Font("assets/fonts/comic.TTF", 30)
 
-	# function for drawing text
+    # function for drawing text
 
-	def draw_text(text, font, text_col, x, y):
-		img = font.render(text, True, text_col)
-		screen.blit(img, (x, y))
+    def draw_text(text, font, text_col, x, y):
+        img = font.render(text, True, text_col)
+        screen.blit(img, (x, y))
 
-	# function for drawing background
+    # function for drawing background
 
-	def draw_bg():
-		scaled_bg = pygame.transform.scale(
-			bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
-		screen.blit(scaled_bg, (0, 0))
+    def draw_bg():
+        scaled_bg = pygame.transform.scale(
+            bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.blit(scaled_bg, (0, 0))
 
-	# function for drawing fighter health bars
+    # function for drawing fighter health bars
 
-	def draw_health_bar(health, x, y):
-		ratio = health / 100
-		pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
-		pygame.draw.rect(screen, RED, (x, y, 400, 30))
-		pygame.draw.rect(screen, GREEN, (x, y, 400 * ratio, 30))
+    def draw_health_bar(health, x, y):
+        ratio = health / 100
+        pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+        pygame.draw.rect(screen, RED, (x, y, 400, 30))
+        pygame.draw.rect(screen, GREEN, (x, y, 400 * ratio, 30))
 
-	# create two instances of fighters
-	fighter_1 = Fighter(1, 200, 310, False, SHAGGY_DATA,
-						SHAGGY_sheet, SHAGGY_ANIMATION_STEPS, [punch_fx, punch_fx])
-	fighter_2 = Fighter(2, 700, 310, True, HORSEMAN_DATA,
-						HORSEMAN_sheet, HORSEMAN_ANIMATION_STEPS, [sword_fx, magic_fx])
+    # create two instances of fighters
+    fighter_1 = Fighter(1, 200, 310, False, SHAGGY_DATA,
+                        SHAGGY_sheet, SHAGGY_ANIMATION_STEPS, [punch_fx, punch_fx])
+    fighter_2 = Fighter(2, 700, 310, True, HORSEMAN_DATA,
+                        HORSEMAN_sheet, HORSEMAN_ANIMATION_STEPS, [sword_fx, magic_fx])
 
-	# game loop
-	run = True
-	while run:
-		clock.tick(FPS)
+    # game loop
+    run = True
+    while run:
+        clock.tick(FPS)
 
-		# draw background
-		draw_bg()
+        # draw background
+        draw_bg()
 
-		# show player stats
-		draw_health_bar(fighter_1.health, 20, 20)
-		draw_health_bar(fighter_2.health, 580, 20)
-		draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
-		draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+        # show player stats
+        draw_health_bar(fighter_1.health, 20, 20)
+        draw_health_bar(fighter_2.health, 580, 20)
+        draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
+        draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
 
-		# update countdown
-		if intro_count <= 0:
-			# move fighters
-			fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT,
-						   screen, fighter_2, round_over)
-			fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT,
-						   screen, fighter_1, round_over)
-		else:
-			# display count timer
-			draw_text(str(intro_count), count_font, RED,
-					  SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
-			# update count timer
-			if (pygame.time.get_ticks() - last_count_update) >= 1000:
-				intro_count -= 1
-				last_count_update = pygame.time.get_ticks()
+        # update countdown
+        if intro_count <= 0:
+            # move fighters
+            fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT,
+                           screen, fighter_2, round_over)
+            fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT,
+                           screen, fighter_1, round_over)
+        else:
+            # display count timer
+            draw_text(str(intro_count), count_font, RED,
+                      SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+            # update count timer
+            if (pygame.time.get_ticks() - last_count_update) >= 1000:
+                intro_count -= 1
+                last_count_update = pygame.time.get_ticks()
 
-		# update fighters
-		fighter_1.update()
-		fighter_2.update()
+        # update fighters
+        fighter_1.update()
+        fighter_2.update()
 
-		# draw fighters
-		fighter_1.draw(screen)
-		fighter_2.draw(screen)
+        # draw fighters
+        fighter_1.draw(screen)
+        fighter_2.draw(screen)
 
-		# check for player defeat
-		if round_over == False:
-			if fighter_1.alive == False:
-				score[1] += 1
-				round_over = True
-				round_over_time = pygame.time.get_ticks()
-			elif fighter_2.alive == False:
-				score[0] += 1
-				round_over = True
-				round_over_time = pygame.time.get_ticks()
-		else:
-			if (rounds != -1):
-				if (score[0] >= rounds | score[1] >= rounds):
-					run = False
-					return
+        # check for player defeat
+        if round_over == False:
+            if fighter_1.alive == False:
+                score[1] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+            elif fighter_2.alive == False:
+                score[0] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+        else:
+            if (rounds != -1):
+                if (score[0] >= rounds or score[1] >= rounds):
+                    run = False
+                    return
 
-			# display victory image
-			screen.blit(victory_img, (360, 150))
+            # display victory image
+            screen.blit(victory_img, (360, 150))
 
-			if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
-				round_over = False
-				intro_count = 3
-				fighter_1 = Fighter(1, 200, 310, False, SHAGGY_DATA,
-									SHAGGY_sheet, SHAGGY_ANIMATION_STEPS, [punch_fx, punch_fx])
-				fighter_2 = Fighter(2, 700, 310, True, HORSEMAN_DATA,
-									HORSEMAN_sheet, HORSEMAN_ANIMATION_STEPS, [sword_fx, magic_fx])
+            if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+                round_over = False
+                intro_count = 3
+                fighter_1 = Fighter(1, 200, 310, False, SHAGGY_DATA,
+                                    SHAGGY_sheet, SHAGGY_ANIMATION_STEPS, [punch_fx, punch_fx])
+                fighter_2 = Fighter(2, 700, 310, True, HORSEMAN_DATA,
+                                    HORSEMAN_sheet, HORSEMAN_ANIMATION_STEPS, [sword_fx, magic_fx])
 
-		# event handler
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
+        # event handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-		# update display
-		pygame.display.update()
+        # update display
+        pygame.display.update()
 
-	pygame.quit()
-	exit()
+    pygame.quit()
+    exit()
